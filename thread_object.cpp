264d6ce1bbebb82e2b99bd0d624d1d36e849da9d
daotung8801 <56082813+daotung8801@users.noreply.github.com>
2020-05-09 10:54:44 +0700

    Update thread_object.cpp

--- thread_object.cpp
+++ thread_object.cpp
@@ -1,8 +1,8 @@
 #include "thread_object.h"
 
 EGG::EGG() {
-    x=rand()%840+100,y=80;
-    speed_y=rand()%5+2;
+    x=rand()%840+60,y=80;
+    speed_y=rand()%4+3;
     thread_rect.h=THREAD_HEIGHT;
     thread_rect.w=THREAD_WIDTH;
     thread_rect.x=x;
@@ -17,10 +17,10 @@ void EGG::egg_move(SDL_Rect main_rect) {
         miss_egg=1;
         }
         if(checkCollision(main_rect,thread_rect)) {point=1;}
-        x=rand()%840+100;
+        x=rand()%840+60;
         thread_rect.x=x;
         y=80;
         thread_rect.y=y;
-        speed_y=rand()%3+3;
+        speed_y=rand()%4+3;
     }
 }
